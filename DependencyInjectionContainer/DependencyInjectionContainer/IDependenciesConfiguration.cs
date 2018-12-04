7e4952bd2d9f1f7806180e469f84fe8d831037ed
Aliaksei <alexx333by@gmail.com>
2018-12-04 14:00:32 +0300

    Inerfaces added

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/IDependenciesConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DependencyInjectionContainer
+{
+    public interface IDependenciesConfiguration
+    {
+        void Register<TDependency, TImplementation>(bool isSingleton = false, string name = null)
+            where TDependency : class
+            where TImplementation : TDependency;
+    }
+}
