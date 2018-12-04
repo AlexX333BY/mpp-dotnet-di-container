f33122e816056a738d4e902d71e60b0d4ac8b888
Aliaksei <alexx333by@gmail.com>
2018-12-04 14:15:31 +0300

    Added open generics generation method

--- DependencyInjectionContainer/DependencyInjectionContainer/IDependenciesConfiguration.cs
+++ DependencyInjectionContainer/DependencyInjectionContainer/IDependenciesConfiguration.cs
@@ -1,9 +1,13 @@
-﻿namespace DependencyInjectionContainer
+﻿using System;
+
+namespace DependencyInjectionContainer
 {
     public interface IDependenciesConfiguration
     {
         void Register<TDependency, TImplementation>(bool isSingleton = false, string name = null)
             where TDependency : class
             where TImplementation : TDependency;
+
+        void Register(Type dependency, Type implementation, string name = null);
     }
 }
