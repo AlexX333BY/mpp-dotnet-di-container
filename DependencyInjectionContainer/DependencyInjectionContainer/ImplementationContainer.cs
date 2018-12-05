7eb146adc84ab9d249f8fd65120b6945a8c8b994
Aliaksei <alexx333by@gmail.com>
2018-12-05 13:14:29 +0300

    Implementation container implemented

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/ImplementationContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DependencyInjectionContainer
+{
+    internal class ImplementationContainer
+    {
+        Type ImplementationType
+        { get; }
+
+        bool IsSingleton
+        { get; }
+
+        object SingletonInstance
+        { get; set; }
+
+        string Name
+        { get; }
+
+        bool IsOpenGeneric
+        { get; }
+
+        internal ImplementationContainer(Type implementationType, bool isSingleton, string name, bool isOpenGeneric)
+        {
+            ImplementationType = implementationType;
+            IsSingleton = isSingleton;
+            Name = name;
+            IsOpenGeneric = isOpenGeneric;
+        }
+    }
+}
