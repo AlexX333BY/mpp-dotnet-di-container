291c84b4ccbcef465a498942134c3eef08d8ca89
Aliaksei <alexx333by@gmail.com>
2018-12-05 14:59:08 +0300

    Added different types generation separating

--- DependencyInjectionContainer/DependencyInjectionContainer/DependencyProvider.cs
+++ DependencyInjectionContainer/DependencyInjectionContainer/DependencyProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,47 @@ namespace DependencyInjectionContainer
 
         public IEnumerable<TDependency> Resolve<TDependency>(string name = null) where TDependency : class
         {
+            if (typeof(TDependency).IsValueType)
+            {
+                throw new ArgumentException("Cannot create non-reference dependencies");
+            }
+
+            return (IEnumerable<TDependency>)Resolve(typeof(TDependency), name);
+        }
+
+        protected IEnumerable<object> Resolve(Type dependency, string name)
+        {
+            if (dependency.IsGenericType || dependency.IsGenericTypeDefinition)
+            {
+                return ResolveGeneric(dependency, name);
+            }
+            else
+            {
+                return ResolveNonGeneric(dependency, name);
+            }
+        }
+
+        protected IEnumerable<object> ResolveGeneric(Type dependency, string name)
+        {
             throw new NotImplementedException();
+        }
+
+        protected IEnumerable<object> ResolveNonGeneric(Type dependency, string name)
+        {
+            if (dependency.IsValueType)
+            {
+                return new List<object>
+                {
+                    Activator.CreateInstance(dependency)
+                };
+            }
+
+            throw new NotImplementedException();
+        }
+
+        protected ConstructorInfo GetConstructor(Type type)
+        {
+            return type.GetConstructors().OrderBy((constructor) => constructor.GetParameters().Length).FirstOrDefault();
         }
 
         public DependencyProvider(IDependenciesConfiguration configuration)
