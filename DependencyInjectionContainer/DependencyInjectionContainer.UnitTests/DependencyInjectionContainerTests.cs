42b0ece59c4f651f01312e4b5626e18b95e99f60
Aliaksei <alexx333by@gmail.com>
2018-12-07 11:18:00 +0300

    Tests base added

---
+++ DependencyInjectionContainer/DependencyInjectionContainer.UnitTests/DependencyInjectionContainerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyInjectionContainer.UnitTests
+{
+    [TestClass]
+    public class DependencyInjectionContainerTests
+    {
+        IDependenciesConfiguration config;
+        IDependencyProvider provider;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            config = new DependenciesConfiguration();
+        }
+
+        [TestMethod]
+        public void NonGenericTypeRegisterTest()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GenericTypeRegisterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OpenGenericTypeRegisterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void NonGenericTypeResolveTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GenericTypeResolveTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OpenGenericTypeResolveTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void SingletonResolveTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ExplicitNameResolveTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ConstructorNameResolveTest()
+        {
+
+        }
+    }
+}
