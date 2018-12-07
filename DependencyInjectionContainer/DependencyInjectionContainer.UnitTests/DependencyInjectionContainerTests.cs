0096aaa2a50476da986ccd2e76ccf2b5b0d299e2
Aliaksei <alexx333by@gmail.com>
2018-12-07 11:51:02 +0300

    Register tests implemented

--- DependencyInjectionContainer/DependencyInjectionContainer.UnitTests/DependencyInjectionContainerTests.cs
+++ DependencyInjectionContainer/DependencyInjectionContainer.UnitTests/DependencyInjectionContainerTests.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyInjectionContainer.UnitTests.AccessoryClasses;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace DependencyInjectionContainer.UnitTests
 {
@@ -17,19 +21,52 @@ namespace DependencyInjectionContainer.UnitTests
         [TestMethod]
         public void NonGenericTypeRegisterTest()
         {
-            
+            config.Register<IMyInterface, MyImplementation1>();
+            config.Register<IMyInterface, MyImplementation2>();
+            var registeredImplementations = config.GetImplementations(typeof(IMyInterface)).ToList();
+            Assert.AreEqual(2, registeredImplementations.Count);
+
+            List<Type> expectedRegisteredTypes = new List<Type>
+            {
+                typeof(MyImplementation1),
+                typeof(MyImplementation2)
+            };
+            CollectionAssert.AreEquivalent(expectedRegisteredTypes, 
+                registeredImplementations.Select((implementation) => implementation.ImplementationType).ToList());
         }
 
         [TestMethod]
         public void GenericTypeRegisterTest()
         {
+            config.Register<IMyGenericInterface<IMyInterface>, MyGenericImplementation1<IMyInterface>>();
+            config.Register<IMyGenericInterface<IMyInterface>, MyGenericImplementation2<IMyInterface>>();
+            var registeredImplementations = config.GetImplementations(typeof(IMyGenericInterface<IMyInterface>)).ToList();
+            Assert.AreEqual(2, registeredImplementations.Count);
 
+            List<Type> expectedRegisteredTypes = new List<Type>
+            {
+                typeof(MyGenericImplementation1<IMyInterface>),
+                typeof(MyGenericImplementation2<IMyInterface>)
+            };
+            CollectionAssert.AreEquivalent(expectedRegisteredTypes,
+                registeredImplementations.Select((implementation) => implementation.ImplementationType).ToList());
         }
 
         [TestMethod]
         public void OpenGenericTypeRegisterTest()
         {
+            config.Register(typeof(IMyGenericInterface<>), typeof(MyGenericImplementation1<>));
+            config.Register(typeof(IMyGenericInterface<>), typeof(MyGenericImplementation2<>));
+            var registeredImplementations = config.GetImplementations(typeof(IMyGenericInterface<>)).ToList();
+            Assert.AreEqual(2, registeredImplementations.Count);
 
+            List<Type> expectedRegisteredTypes = new List<Type>
+            {
+                typeof(MyGenericImplementation1<>),
+                typeof(MyGenericImplementation2<>)
+            };
+            CollectionAssert.AreEquivalent(expectedRegisteredTypes,
+                registeredImplementations.Select((implementation) => implementation.ImplementationType).ToList());
         }
 
         [TestMethod]
